97f7d2eb7d4208608318d3d20710864bd490be49
denis <denis.scarlat@gmail.com>
2021-04-16 19:37:07 +0300

    added scripts for deploy

--- pages/404.js
+++ pages/404.js
@@ -3,7 +3,7 @@ import MainContainer from "../components/MainContainer"
 
 export default function Error() {
   return (
-    <MainContainer>
+    <MainContainer keywords={"Error page"}>
       <h1>My custom error page</h1>
     </MainContainer>
   );
